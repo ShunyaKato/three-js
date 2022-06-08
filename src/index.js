246e17264853bde050f9099c768349ffe05cf85e
draw4-kato <Kato_s@draw4.co.jp>
2022-06-08 19:31:59 +0900

    add train04

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './js/three_test'
 import './js/three_train01'
 import './js/three_train02'
 import './js/three_train03'
+import './js/three_train04'
 import './scss/style.scss'
 
 // const jiro = new Hello('Jiro')
