ef30e3a22032d1a5616e80e2e9d2351020a48331
draw4-kato <Kato_s@draw4.co.jp>
2022-06-08 11:51:31 +0900

    webpack add

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// development モードか否か？
+const isDev = process.env.NODE_ENV === "development";
+
+/** ↓ エディタで補完を効かせるための JSDoc */
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  // モードの切り替え
+  mode: isDev ? "development" : "production",
+  // dev モードではソースマップをつける
+  devtool: isDev ? "source-map" : undefined,
+  devServer: {
+    static: {
+      directory: "./dist",
+    },
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 js のファイル（正規表現）
+        test: /\.js$/,
+        // ローダーの指定
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
