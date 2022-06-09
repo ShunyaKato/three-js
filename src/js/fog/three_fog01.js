4c3b0a787b5924b8a4a19b5da502e3ff61b9e858
draw4-kato <Kato_s@draw4.co.jp>
2022-06-09 22:33:24 +0900

    fog01 add

--- src/js/fog/three_fog01.js
+++ src/js/fog/three_fog01.js
@@ -27,72 +27,36 @@ function init() {
 
   // カメラを作成
   const camera = new THREE.PerspectiveCamera(45, width / height)
+  camera.position.set(0, 0, +1000)
 
+  // グループを作成
+  const group = new THREE.Group()
+  scene.add(group)
+  const geometry = new THREE.BoxBufferGeometry(50, 50, 50)
+  const material = new THREE.MeshStandardMaterial()
 
-  // 光源を作成
-  {
-    const spotLight = new THREE.SpotLight(0xffffff, 4, 2000, Math.PI / 5, 0.2, 1.5)
-    spotLight.position.set(500, 300, 500)
-    spotLight.castShadow = true
-    spotLight.shadow.mapSize.width = 2048
-    spotLight.shadow.mapSize.height = 2048
-    scene.add(spotLight)
+  for (let i = 0; i < 1000; i++) {
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = (Math.random() - 0.5) * 2000
+    mesh.position.y = (Math.random() - 0.5) * 2000
+    mesh.position.z = (Math.random() - 0.5) * 2000
+    mesh.rotation.x = Math.random() * 2 * Math.PI
+    mesh.rotation.y = Math.random() * 2 * Math.PI
+    mesh.rotation.z = Math.random() * 2 * Math.PI
+    // グループに格納する
+    group.add(mesh)
   }
 
-  // 地面を作成
-  {
-    // 床のテクスチャー
-    const texture = new THREE.TextureLoader().load('/img/floor.png')
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping // リピート可能に
-    texture.repeat.set(10, 10) // 10x10マスに設定
-    texture.magFilter = THREE.NearestFilter // アンチエイリアスを外す
-
-    const floor = new THREE.Mesh(
-      new THREE.PlaneGeometry(1000, 1000),
-      new THREE.MeshStandardMaterial({
-        map: texture,
-        roughness: 0.0,
-        metalness: 0.6,
-      })
-    )
-    floor.rotation.x = -Math.PI / 2
-    floor.receiveShadow = true
-    scene.add(floor)
-  }
-
-  // マス目を作成
-  {
-    // 立方体のマテリアルとジオメトリを作成
-    const material = new THREE.MeshStandardMaterial({
-      color: 0x22dd22,
-      roughness: 0.1,
-      metalness: 0.2,
-    })
-    const geometry = new THREE.BoxGeometry(45, 45, 45)
-
-    // 立方体を複数作成しランダムに配置
-    for (let i = 0; i < 60; i++) {
-      const box = new THREE.Mesh(geometry, material)
-      box.position.x = Math.round((Math.random() - 0.5) * 19) * 50 + 25
-      box.position.y = 25
-      box.position.z = Math.round((Math.random() - 0.5) * 19) * 50 + 25
-      // 影の設定
-      box.receiveShadow = true
-      box.castShadow = true
-      scene.add(box)
-    }
-  }
+  // 光源
+  scene.add(new THREE.DirectionalLight(0xff0000, 2))
+  scene.add(new THREE.AmbientLight(0x00ffff))
 
   tick();
 
   // 毎フレーム時に実行されるループイベントです
   function tick() {
-    // 角度に応じてカメラの位置を設定
-    camera.position.x = 500 * Math.sin(Date.now() / 2000)
-    camera.position.y = 250
-    camera.position.z = 500 * Math.cos(Date.now() / 2000)
-    // 原点方向を見つめる
-    camera.lookAt(new THREE.Vector3(0, 0, 0))
+    // グループを回す
+    group.rotateY(0.01)
     renderer.render(scene, camera) // レンダリング
     requestAnimationFrame(tick)
   }
