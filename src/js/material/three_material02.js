76f56eaedb1869e0fb32f99975bfef1e3931433d
draw4-kato <Kato_s@draw4.co.jp>
2022-06-09 13:26:39 +0900

    materila js add

---
+++ src/js/material/three_material02.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+
+// ページの読み込みを待つ
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+
+  // サイズを指定
+  const width = 960;
+  const height = 540;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#material02'),
+    antialias: true,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, width / height);
+  camera.position.set(0, 0, +1000);
+
+  // ドーナツを作成
+  const geometry = new THREE.TorusGeometry(300, 100, 64, 100);
+  const material = new THREE.MeshNormalMaterial({ color: 0x6699ff });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  tick();
+
+  // 毎フレーム時に実行されるループイベントです
+  function tick() {
+    mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.01;
+    renderer.render(scene, camera); // レンダリング
+
+    requestAnimationFrame(tick);
+  }
+}
---
+++ src/js/material/three_material04.js
@@ -0,0 +1,61 @@
+import * as THREE from 'three';
+
+// ページの読み込みを待つ
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+
+  // サイズを指定
+  const width = 960;
+  const height = 540;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#material04'),
+    antialias: true,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, width / height);
+  camera.position.set(0, 0, +1000);
+
+  // ドーナツを作成
+  const geometry = new THREE.TorusGeometry(300, 100, 64, 100);
+  const material = new THREE.MeshPhongMaterial({ color: 0x6699ff });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // 平行光源
+  const directionalLight = new THREE.DirectionalLight(0xffffff)
+  directionalLight.position.set(1, 1, 1)
+  scene.add(directionalLight)
+
+  // ポイント光源
+  const pointLight = new THREE.PointLight(0xffffff, 2, 1000)
+  scene.add(pointLight)
+  const pointLightHelper = new THREE.PointLightHelper(pointLight, 30)
+  scene.add(pointLightHelper)
+
+  tick();
+
+  // 毎フレーム時に実行されるループイベントです
+  function tick() {
+    // メッシュを回転させる
+    mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.01
+    // ライトを周回させる
+    pointLight.position.set(
+      500 * Math.sin(Date.now() / 500),
+      500 * Math.sin(Date.now() / 1000),
+      500 * Math.cos(Date.now() / 500),
+    )
+    renderer.render(scene, camera); // レンダリング
+
+    requestAnimationFrame(tick);
+  }
+}
